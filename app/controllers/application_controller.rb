d9ef3895b8fa90c3966db75fa19858d5452961b1
Northstar <1155211153@link.cuhk.edu.hk>
2026-04-11 00:04:12 +0800

    Skip forgery protection in development
    Skip forgery protection in development environment to ease testing.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+  skip_forgery_protection if Rails.env.development?
+  
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 
